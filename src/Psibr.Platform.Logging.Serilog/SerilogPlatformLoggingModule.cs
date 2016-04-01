bf9fedbf268d3e550e3dd0369bd9071ff53fad24
Ovan Crone <ovan@psibr.net>
2016-04-01 12:50:17 -0500

    initial code checkin

---
+++ src/Psibr.Platform.Logging.Serilog/SerilogPlatformLoggingModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Serilog;
+
+namespace Psibr.Platform.Logging.Serilog
+{
+    public class SerilogPlatformLoggingModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterAdapter<ILogger, IPlatformLogger>(serilogLogger =>
+                new SerilogLoggingAdapter(serilogLogger));
+        }
+    }
+}
